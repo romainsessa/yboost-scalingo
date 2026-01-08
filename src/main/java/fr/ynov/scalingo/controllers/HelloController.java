e7e11ad952388bfdfc7b6c62ffa149ab0607997f
romainsessa <romainsessa@gmail.com>
2026-01-08 15:03:55 +0100

    upgrade hello world with env var

--- src/main/java/fr/ynov/scalingo/controllers/HelloController.java
+++ src/main/java/fr/ynov/scalingo/controllers/HelloController.java
@@ -1,14 +1,20 @@
 package fr.ynov.scalingo.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Value("${custom}")
+	private String customEnvVariable;
+	
 	@GetMapping
-	public String helloWorld()  {
-		return "hello world";
+	public String helloWorld(Model model)  {
+		model.addAttribute("customEnvVariable", customEnvVariable);
+		return "index";
 	}
 	
 }
